6d692df65421ffc3ccfee4c071ad275e016054b6
phatcharaphan <phat.pan@gmail.com>
2017-06-14 22:23:59 +0700

    add example

--- js/app.js
+++ js/app.js
@@ -1,41 +1,43 @@
-var myApp = angular.module("myApp", ['ngRoute']);
-myApp.service("mainService", function($http){
-    this.getBooks = function(){
-        return $http.get("http://172.17.12.247:8080/api/getBooks");
-    };
-});
-myApp.config(function($routeProvider){
-    $routeProvider.when("/viewBook",{
-        templateUrl: "view.html"
-    }).when("/home", {
-        templateUrl: "home.html"
-    }).when("/addBook", {
-        templateUrl: "add.html"
-    });
-});
-myApp.controller("mainController", function($scope, $location, mainService){
-    $scope.myName = "kae";
-    $scope.isShow = true;
-    $scope.toDay = new Date();
-    $scope.getBooks = function(){
-        mainService.getBooks()
-            .then(function(response){
-                $scope.books = response.data.Result;
+
+angular.module("myApp", ['ngRoute'])
+    .service("mainServer", function($http){
+        this.getBooks = function(){
+            return $http.get("http://localhost:8080/api/getBooks");
+        };
+    })
+    .config(function($routeProvider){
+        $routeProvider.when("/viewBook", {
+            templateUrl: "view.html"
+        }).when("/addBook", {
+            templateUrl: "addBook.html"
+        }).when("/home", {
+            templateUrl: "home.htm"
+        }).otherwise({
+            redirectTo: "/home"
+        });
+    })
+    .controller("mainController", function($scope, $location, mainServer){
+        $scope.myName = "Kae";
+        $scope.isShow = true;
+        $scope.toDay = new Date();
+        $scope.isShowMenu = true;
+        $scope.saveBook = function(data){
+            mainServer.getBooks().then(function(data){
+                console.log(data);
             });
-    };
-    $scope.saveBook = function(data){
-        $scope.books.push(data);
-        $location.path("/viewBook");
-    };
-    // $scope.books = [{
-    //     name: "AngularJS",
-    //     price: 299,
-    //     description: "Angular JS Basic",
-    //     stock: false
-    // },{
-    //     name: "Golang",
-    //     price: 499,
-    //     description: "Golang basic",
-    //     stock: true
-    // }];
-});
+            $scope.books.push(data);
+            $location.path("/viewBook");
+            $scope.data = [];
+        };
+        $scope.books = [
+        {
+            name: 'AngularJs',
+            price: 899,
+            description: 'angular js basic'
+        },
+        {
+            name: 'Robot framework',
+            price: 599,
+            description: 'automate test using robot framework'
+        }];
+    });
